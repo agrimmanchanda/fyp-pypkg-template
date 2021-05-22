ca7977b25cfbaf8f76d3a5272c137ca56eb4e89f
Agrim Manchanda <agrim.manchanda@hotmail.co.uk>
2021-05-22 19:44:28 +0100

    BNRegressor v0.1 added

--- pkgname/core/bayes_net.py
+++ pkgname/core/bayes_net.py
@@ -6,6 +6,9 @@ import numpy as np
 from pgmpy.models import BayesianModel
 from pgmpy.estimators import HillClimbSearch, BicScore
 
+# Libraries sklearn
+from sklearn.base import BaseEstimator, RegressorMixin
+
 def learn_model_structure(data):
 
     hc = HillClimbSearch(data, scoring_method=BicScore(data))
@@ -120,3 +123,81 @@ class BNImputer(BayesianModel):
                     )
 
         return pred_values
+
+
+class BNRegressor(BayesianModel, BaseEstimator, RegressorMixin):
+    
+    def __init__(self, PANEL):
+        
+        self.PANEL = PANEL
+        
+        super().__init__(self)
+        
+    def _learn_model_structure(self, X,y=None):
+        
+        if y is not None:
+            print(f'lstr {type(X)}')
+            data = pd.concat([X,y], axis=1)
+        else:
+            data = X
+
+        hc = HillClimbSearch(data, scoring_method=BicScore(data))
+
+        best_model = hc.estimate()
+        
+        bn = BayesianModel(best_model.edges)
+        
+        super().__init__(bn.edges)
+    
+    def _fit_model(self,
+           X, y=None,
+           **kwargs):
+        
+        print("Entering _fit_model...")
+        
+        from pgmpy.estimators import BayesianEstimator
+        
+        estimator = BayesianEstimator
+        
+        if y is not None:
+            data = pd.concat([X,y], axis=1)
+        else:
+            data = X
+        
+        data = data[self.PANEL]
+        
+        print(f"\n {data}")
+        
+        super().fit(data, estimator, **kwargs)
+    
+    def predict(self, X):
+        
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X, columns=self.obs_vars)
+        
+        aux = X.copy()
+        
+        ypred = super().predict(X)
+        
+        return ypred.values
+    
+    def fit(self, X, y=None):
+        
+        self.obs_vars = []
+        
+        if y is not None:
+            self.col_drop = y.name
+            self.obs_vars = set(self.PANEL) - set([y.name])
+            y = y.reset_index(drop=True)
+        
+        if isinstance(X, np.ndarray):
+            
+            X = pd.DataFrame(X, columns=self.obs_vars)
+        
+        if len(self.edges) > 0 and len(self.nodes) > 0:
+            self._fit_model(X,y)
+        else:
+            self._learn_model_structure(X,y)
+            self._fit_model(X,y)
+        
+        return self
